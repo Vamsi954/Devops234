048efa83f01a607a7f6db9933a426583c0c99650
Vamsi Krishna <you@example.com>
2023-03-27 10:39:32 +0530

    Modified sort file

--- sort.py
+++ sort.py
@@ -4,7 +4,8 @@ def bubblesort(arr):
         for j in range(0, n-i-1):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
-arr = [2,4,1,10]
+arr = [2, 3, 5,1]
 bubblesort(arr)
+print("The sorted order is: ")
 for i in range(len(arr)):
     print(arr[i])
